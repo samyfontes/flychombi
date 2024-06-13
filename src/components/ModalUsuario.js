6a7dae8e65252c3797d6dbcea9ab8bb78c54179f
Samuel Fontes <samuelfontes.coder@gmail.com>
2024-06-13 16:29:11 -0300

    modal style modifications

--- src/components/ModalUsuario.js
+++ src/components/ModalUsuario.js
@@ -15,15 +15,13 @@ const ModalUsuario = ({ isOpen, onClose, user }) => {
         }
     };
 
-    console.log('User:', user);
-
     return (
         <Dialog 
             open={isOpen} 
             onClose={onClose}
             PaperProps={{
                 style: {
-                    backgroundColor: '#fdbe0f',
+                    backgroundColor: 'rgb(255, 253, 253)',
                     boxShadow: '0 0 10px rgba(0, 0, 0, 0.25)',
                     borderRadius: '10px',
                     padding: '20px'
@@ -35,9 +33,9 @@ const ModalUsuario = ({ isOpen, onClose, user }) => {
                     Hola, {user.name}
                 </Typography>
             </DialogTitle>
-            <DialogContent>
-                <div className="contenedor-medio" style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <div className="contenedor-izquierda" style={{ flex: 1, marginRight: '10px' }}>
+            <DialogContent dividers>
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                    <div>
                         <Typography variant="body1" style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#333' }}>
                             Información del usuario
                         </Typography>
@@ -60,39 +58,14 @@ const ModalUsuario = ({ isOpen, onClose, user }) => {
                             {user.phoneNumber}
                         </Typography>
                     </div>
-                    <div className="contenedor-derecha" style={{ flex: 1, marginLeft: '10px' }}>
+                    <div>
                         <Typography variant="body1" style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#333' }}>
                             Próximos vuelos
-                        </Typography>
-                        <Typography variant="body2" style={{ fontFamily: 'Arial, Helvetica, sans-serif', color: '#333' }}>
-
                         </Typography>
                     </div>
                 </div>
             </DialogContent>
             <DialogActions>
-                <Button 
-                    onClick={onClose} 
-                    style={{
-                        backgroundColor: '#ffa800',
-                        color: 'black',
-                        borderRadius: '20px',
-                        fontFamily: 'Arial, Helvetica, sans-serif',
-                        margin: '5px',
-                        transition: 'transform 0.2s',
-                        boxShadow: '5px 5px 10px rgb(49, 49, 49)'
-                    }}
-                    onMouseOver={(e) => {
-                        e.target.style.backgroundColor = '#d78f00';
-                        e.target.style.transform = 'scale(1.1)';
-                    }}
-                    onMouseOut={(e) => {
-                        e.target.style.backgroundColor = '#ffa800';
-                        e.target.style.transform = 'scale(1)';
-                    }}
-                >
-                    Volver
-                </Button>
                 <Button 
                     href="/" 
                     style={{
